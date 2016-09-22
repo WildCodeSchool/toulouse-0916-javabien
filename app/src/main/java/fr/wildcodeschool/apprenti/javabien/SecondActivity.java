08bd9aeb747939e11dbf084246efd00f5c719162
helenavan <tedone.bd@gmail.com>
2016-09-22 15:49:11 +0200

    second activite java

--- app/src/main/java/fr/wildcodeschool/apprenti/javabien/SecondActivity.java
+++ app/src/main/java/fr/wildcodeschool/apprenti/javabien/SecondActivity.java
@@ -1,7 +1,9 @@
 package fr.wildcodeschool.apprenti.javabien;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-    }
+
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter(new ButtonAdapter(this));
+
+      /**  gridView.setOnClickListener((parent, view, position, id)) {
+            Intent secondActivite = new Intent(SecondActivity.this, ExoActivity)**/
+        }
+
+
+
+    
+    /**@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }**/
 }
