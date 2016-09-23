c285994d13156cc97228810b88588aa232915b55
tuffery <furtuff@gmail.com>
2016-09-23 11:31:55 +0200

    avec ce fichier ça marche mieux

---
+++ app/src/main/java/fr/wildcodeschool/apprenti/javabien/TItem.java
@@ -0,0 +1,24 @@
+package fr.wildcodeschool.apprenti.javabien;
+
+/**
+ * Created by tuffery on 22/09/16.
+ */
+
+public class TItem {
+    private int blob;
+    private String chok;
+
+    public TItem(int blob, String chok){
+        blob = this.blob;
+        chok = this.chok;
+
+    }
+
+    public int getBlob() {
+        return blob;
+    }
+
+    public String getChok() {
+        return chok;
+    }
+}
