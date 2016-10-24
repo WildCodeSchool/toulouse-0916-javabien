96443dc53f04bda961bfa18db10711b595e88cce
tuffery <furtuff@gmail.com>
2016-10-24 12:14:14 +0200

    notifspam

--- app/src/main/java/fr/wildcodeschool/apprenti/javabien/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/apprenti/javabien/MainActivity.java
@@ -1,10 +1,13 @@
 package fr.wildcodeschool.apprenti.javabien;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +21,8 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import fr.wildcodeschool.apprenti.javabien.Model.Contenant;
@@ -32,6 +37,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+// programation des spamnotifs
+notificateur(getApplicationContext());
 
         // logo cerf redirigeant vers le site wildcodeschool.fr
        /* ImageView logo = (ImageView)findViewById(R.id.logowild);
@@ -118,9 +126,60 @@ public class MainActivity extends Activity {
 
     }
 
+private void notificateur(Context context){
+    // lancement de la programmation du spam notification
+
+    //recupération de la date actuelle
+    Date date = new Date();
+    Calendar calendar = Calendar.getInstance();
+    calendar.getTimeInMillis();
+
+    // ajout de 6 jours
+    calendar.add(Calendar.DATE, 6);
+// creation de l'intent qui lance le service de notification
+    Intent intent = new Intent(MainActivity.this,NotificationReceiver.class);
+// creation du Pendingintent pour l'alarmManager avec l'intent qui lance la notification
+    PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this,
+            0, intent, 0);
+// initialisation de l'alarmManager
+    AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+/*
+// lancement de l'alarmManager
+   */
+ if(Build.VERSION.SDK_INT < 23) {
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                pendingIntent);
+    }else {
+
+        Intent i = new Intent(context, NotificationReceiver.class);
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
+        Intent i2 = new Intent(context, Notifyme.class);
+        PendingIntent pi2 = PendingIntent.getActivity(context, 0, i2, 0);
+
+        AlarmManager.AlarmClockInfo ac =
+                new AlarmManager.AlarmClockInfo(calendar.getTimeInMillis(),
+                        pi2);
+
+        alarmManager.setAlarmClock(ac,pendingIntent);
+     alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
+    }/*
+
+    // Ajout d'un spamNotif ultérieur
+    //ajout de 6 jours au calendrier précédent
+  /*  calendar.add(Calendar.DAY_OF_MONTH, 6);
+    //et rebelote
+    AlarmManager alarmManager2 = (AlarmManager) getSystemService(ALARM_SERVICE);
+    alarmManager2.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+            pendingIntent);
+
+*/
+}
+
+public void onStart(){
+    super.onStart();
 
 
-
+}
 
 
 }
